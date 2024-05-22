8ab6f492ce51317e93ca0110d666be06f9075f19
Can Boru <s.canboru@gmail.com>
2024-05-22 05:28:04 +0300

    feat(*): css files edited

--- Client/src/pages/MainPage.jsx
+++ Client/src/pages/MainPage.jsx
@@ -10,6 +10,10 @@ import megaphone_red from "../assets/megaphone_red.png";
 import megaphone_white from "../assets/megaphone_white.png";
 import app_form_red from "../assets/app_form_red.png";
 import app_form_white from "../assets/app_form_white.png";
+import new_app_form_white from "../assets/new_app_form_white.png";
+import new_app_form_red from "../assets/new_app_form_red.png";
+import briefcase_red from "../assets/briefcase_red.png";
+import briefcase_white from "../assets/briefcase_white.png";
 
 /* TEMP PART */
 import user_icon from "../assets/user.png";
@@ -17,13 +21,15 @@ import user_icon from "../assets/user.png";
 function MainPage() {
     return (
         <div className="main_page_div">
-            <Header username="ünal dalkılıç" profile_icon={user_icon} profile_link=""/>
+            <Header username="ünal dalkılıç" profile_icon={user_icon} profile_link="" />
             <Navbar navbar_items={[
-                {icon: document_red, hover_icon: document_white, text: "Documents"},
-                {icon: megaphone_red, hover_icon: megaphone_white, text: "Notifications"},
-                {icon: app_form_red, hover_icon: app_form_white, text: "Applications"},
+                { icon: document_red, hover_icon: document_white, text: "Documents" },
+                { icon: megaphone_red, hover_icon: megaphone_white, text: "Announcements" },
+                { icon: app_form_red, hover_icon: app_form_white, text: "Applications" },
+                { icon: new_app_form_red, hover_icon: new_app_form_white, text: "New Application" },
+                { icon: briefcase_red, hover_icon: briefcase_white, text: "Opportunities" },
                 // Add others
-            ]}/>
+            ]} />
             <Footer />
         </div>
     )
